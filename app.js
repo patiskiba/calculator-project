f66b9cb341c13d3046c0da6c63d4fec736259c43
patiskiba <patricia.skiba@mscoding.org>
2025-05-22 15:00:46 -0500

    started javascript

---
+++ app.js
@@ -0,0 +1,8 @@
+let displayScreen = document.getElementById("displayInput");
+
+//! Buttons
+const buttons = document.querySelectorAll(".buttonsRow input");
+console.log(buttons);
+
+const clearButton = document.getElementById("ac");
+const deleteButton = document.getElementById("delete");
