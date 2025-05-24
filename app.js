36bb9ad4e75b239ce77436510dfb198eabca70ce
patiskiba <patricia.skiba@mscoding.org>
2025-05-23 23:06:35 -0500

    Need conditionals for clear,equals,del

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 let displayScreen = document.getElementById("displayInput");
 
+let calculation = [];
+let displayedCalculation;
+
 //! Buttons
 const buttons = document.querySelectorAll(".buttonsRow input");
 console.log(buttons);
 
+//! Button clicking events
 for (let button of buttons) {
     button.addEventListener("click", () => {
-        
+        console.log(`Clicked ${button.value}`);
+
+        //? Each button press adds to calculation array
+        calculation.push(button.value);
+        console.log(calculation);
+
+        //? Calculation array turned into string
+        displayedCalculation = calculation.join("");
+        console.log(displayedCalculation);
+
+        //? Actually display calculation on calculator
+        displayScreen.value = displayedCalculation;
     })
 }
